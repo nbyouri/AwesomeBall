4b6daeb5d0ab701d98489d350cb2be8ea526ce89
yrmt <youri.mout@gmail.com>
2014-11-10 18:01:33 +0100

    Change button color when pressed.

--- AwesomeBall/src/test_jeu/Button.java
+++ AwesomeBall/src/test_jeu/Button.java
@@ -4,22 +4,30 @@ import java.awt.Color;
 import java.awt.Font;
 import java.awt.event.ActionListener;
 
+
 import javax.swing.JButton;
 import javax.swing.border.Border;
 import javax.swing.border.LineBorder;
+import javax.swing.event.ChangeEvent;
+import javax.swing.event.ChangeListener;
+
 
 @SuppressWarnings("serial")
 public class Button extends JButton {
 	private Font buttonFont;
 	private Color buttonFgColor;
 	private Color buttonFbColor;
+	private Color buttonPressedFgColor;
 	private Border buttonBorder;
+	private Border buttonPressedBorder;
 	public Button(String title, ActionListener al) {
 		super(title);
 		
 		buttonFont = new Font("Helvetica", Font.BOLD, 12);
 		buttonBorder = new LineBorder(Color.cyan, 1);
+		buttonPressedBorder = new LineBorder(Color.yellow, 1);
 		buttonFgColor = Color.cyan;
+		buttonPressedFgColor = Color.yellow;
 		buttonFbColor = Color.black;
 		
 		this.setFont(this.buttonFont);
@@ -29,5 +37,15 @@ public class Button extends JButton {
 		this.setFocusPainted(false);
 		this.setContentAreaFilled(false);
 		this.addActionListener(al);
+		this.addChangeListener(new ChangeListener() {
+			@Override
+			public void stateChanged(ChangeEvent evt) {
+				if (getModel().isPressed()) {
+					// don't use this here
+					setForeground(buttonPressedFgColor);
+					setBorder(buttonPressedBorder);
+				}
+			}
+		});
 	}
 }
