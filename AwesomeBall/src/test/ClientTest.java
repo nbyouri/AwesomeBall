d772df773e19e648b2ca8e0277db82920b656772
yrmt <youri.mout@gmail.com>
2014-12-12 20:13:41 +0100

    move test to appropriate place

--- AwesomeBall/src/test/ClientTest.java
+++ AwesomeBall/src/test/ClientTest.java
@@ -1,4 +1,4 @@
-package net;
+package test;
 
 import java.io.BufferedReader;
 import java.io.IOException;
@@ -6,6 +6,8 @@ import java.io.InputStreamReader;
 import java.io.PrintWriter;
 import java.net.Socket;
 import java.net.UnknownHostException;
+
+import net.initServer;
 
 public class ClientTest extends Socket implements Runnable {
 	
